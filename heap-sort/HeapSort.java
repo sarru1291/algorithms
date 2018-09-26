3d8cb78ffaf4adf4eb6fa86e3c644acd6345c6a5
sarru1291 <sarru1291@gmail.com>
2018-09-26 07:20:03 +0530

    heapsort code written

---
+++ heap-sort/HeapSort.java
@@ -0,0 +1,65 @@
+public class HeapSort {
+
+    static void printArray(int array[]) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+    }
+
+    static void heapSort(int[] array) {
+        int i, temp;
+        for (i = (array.length / 2) - 1; i >= 0; i--) {
+            buildHeap(array, array.length, i);
+        }
+        for (i = array.length - 1; i >= 0; i--) {
+            temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            buildHeap(array, i, 0);
+        }
+    }
+
+    static void buildHeap(int[] arr, int size, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < size && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        if (right < size && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            buildHeap(arr, size, largest);
+        }
+    }
+
+    public static void main(String... input) {
+        int size = Integer.parseInt(input[0]); // taking size of array from command line argument
+        int array[] = new int[size];
+
+        // System.err.println("Worst case:Array");
+        // for(int s=0;s<size;s++){
+        // array[s]=array[size-i-1];
+        // }
+        // System.err.println("Best case:Array");
+        // for(int s=0;s<size;s++){
+        // array[s]=s;
+        // }
+        System.err.println("Average case:Array");
+        for (int s = 0; s < size; s++) {
+            array[s] = (int) (Math.random() * 10000 + 1);
+        }
+        printArray(array);
+        heapSort(array);
+        System.out.println("Sorted Array: ");
+        printArray(array);
+    }
+}
