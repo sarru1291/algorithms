6280d1defb3d729ec3aeab519218a92bb249a889
sarru1291 <sarru1291@gmail.com>
2018-10-03 17:54:04 +0530

    mcm added

--- matrix-chain-multiplication/MatrixChainMultiplication.java
+++ matrix-chain-multiplication/MatrixChainMultiplication.java
@@ -1,50 +1,55 @@
-public class MatrixChainMultiplication {
+class MatrixChainMultiplication {
+    static void printParenthesis(int i, int j, int n, int[][] bracket, char name) {
+        if (i == j) {
+            System.out.print(name++);
 
-    void matrixchain(int a[]) {
-        int q;
-        int n = a.length;
-        int m[][] = new int[n][n];
-        int s[][] = new int[n][n];
-        for (int i = 1; i < n; i++) {
-            m[i][i] = 0;
+            return;
         }
-        for (int l = 2; l < n; l++) {
 
-            for (int i = 1; i < n - l + 1; i++) {
-                int j = i + l - 1;
+        System.out.print("(");
+
+        printParenthesis(i, bracket[i][j], n, bracket, name);
+
+        printParenthesis(bracket[i][j] + 1, j, n, bracket, name);
+
+        System.out.print(")");
+    }
+
+    static void matrixChainOrder(int p[], int n) {
+
+        int m[][] = new int[n][n];
+        int bracket[][] = new int[n][n];
+
+        for (int i = 1; i < n; i++)
+            m[i][i] = 0;
+
+        for (int L = 2; L < n; L++) {
+            for (int i = 1; i < n - L + 1; i++) {
+                int j = i + L - 1;
                 m[i][j] = Integer.MAX_VALUE;
                 for (int k = i; k <= j - 1; k++) {
-                    q = m[i][k] + m[k + 1][j] + a[i - 1] * a[k] * a[j];
+                    int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
                     if (q < m[i][j]) {
                         m[i][j] = q;
-                        s[i][j] = k;
+
+                        bracket[i][j] = k;
                     }
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(m[i][j] + " ");
-            }
-            System.out.println();
-        }
-        print_optimal(s, 1, 6);
+
+        char name = 'A';
+
+        System.out.println("Optimal Parenthesization is : ");
+        printParenthesis(1, n - 1, n, bracket, name);
+        System.out.println("nOptimal Cost is : " + m[1][n - 1]);
     }
 
-    void print_optimal(int s[][], int i, int j) {
-        if (i == j) {
-            System.out.print("A" + i);
-        } else {
-            System.out.print("(");
-            print_optimal(s, i, s[i][j]);
-            print_optimal(s, s[i][j] + 1, j);
-            System.out.print(")");
-        }
-    }
+    public static void main(String... input) {
+        int arr[] = { 40, 20, 30, 10, 30 };
+        int n = arr.length;
+        // int n = sizeof(arr) / sizeof(arr[0]);
+        matrixChainOrder(arr, n);
 
-    public static void main(String args[]) {
-        int a[] = { 30, 35, 15, 5, 10, 20, 25 };
-        MatrixChainMultiplication n = new MatrixChainMultiplication();
-        n.matrixchain(a);
     }
 }
