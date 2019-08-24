e154a8ca6d24c348f025a9d70d5c011528866753
sarru1291 <sarru1291@gmail.com>
2019-08-24 09:32:15 +0530

    call apply bind methods js

---
+++ other problems/javascript/call-apply-bind/callApplyBind.js
@@ -0,0 +1,51 @@
+// call(), apply() and bind() method is used for function borrowing with the provided data value.
+
+let student1={
+    firstName:"bill",
+    lastName:"gates",
+    printFullName:function () {
+        console.log(this.firstName+" "+this.lastName);
+    }
+}
+let student2={
+    firstName:"elon",
+    lastName:"musk"
+}
+student1.printFullName();   // output: bill gates
+student1.printFullName.call(student2);  // output: elon musk
+
+
+// call() and apply() serves the same purpose except the way it accepts the arguments i.e. call() takes arguments individually while apply() takes an array of arguments.
+
+let student1={
+    firstName:"bill",
+    lastName:"gates",
+    printFullName:function (hometown,company) {
+        console.log(this.firstName+" "+this.lastName+" "+hometown+" "+company);
+    }
+}
+let student2={
+    firstName:"elon",
+    lastName:"musk"
+}
+student1.printFullName("seattle","microsoft");  // bill gates seattle microsoft
+student1.printFullName.call(student2,"pretoria","spacex");  //elon musk pretoria spacex
+student1.printFullName.apply(student2,["pretoria","spacex"]);   // elon musk pretoria spacex
+
+
+// bind() method bind the borrowed function to given object by creating a copy as new function that can be invoked later any time.
+
+let student1={
+    firstName:"bill",
+    lastName:"gates",
+    printFullName:function (hometown,company) {
+        console.log(this.firstName+" "+this.lastName+" "+hometown+" "+company);
+    }
+}
+let student2={
+    firstName:"elon",
+    lastName:"musk"
+}
+
+let printFullName2=student1.printFullName.bind(student2,"pretoria","spacex");
+printFullName2();   // elon musk pretoria spacex
